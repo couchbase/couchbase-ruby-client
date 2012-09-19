fc092dbee47fddf1c616055845cf445986e2d7ee
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-09-19 08:30:00 -0700

    RCBC-82 Not all rubies are fat on MacOS
    Change-Id: I6428506cb2490fd77171dd51b96bab0239a599df
    Reviewed-on: http://review.couchbase.org/20959
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- ext/couchbase_ext/extconf.rb
+++ ext/couchbase_ext/extconf.rb
@@ -20,7 +20,7 @@ require 'rbconfig'
 # This hack is more robust, because in bundler environment bundler touches
 # all constants from rbconfig.rb before loading any scripts. This is why
 # RC_ARCHS doesn't work under bundler on MacOS.
-if RUBY_PLATFORM =~ /darwin/
+if RUBY_PLATFORM =~ /darwin/ && defined?(RbConfig::ARCHFLAGS)
   [RbConfig::CONFIG, RbConfig::MAKEFILE_CONFIG].each do |cfg|
     cfg["CFLAGS"].gsub!(RbConfig::ARCHFLAGS, '')
     cfg["LDFLAGS"].gsub!(RbConfig::ARCHFLAGS, '')
