79743e8e4a287353a78500a37abb5e3b97d20f95
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-05-07 11:28:35 +0300

    Update examples
    Added two new examples:
    
    1) Asynchronous API server with Couchbase backend using Goliath and
       Grape framework
    
    2) Gzip transcoder, the demonstration of writing custom client-side
       data filters for Couchbase (in addition to three built-in:
       Document, Marshal and Plain)
    
    Change-Id: Id35f083fe6bf33e34cb914754695e23849a78c11
    Reviewed-on: http://review.couchbase.org/26153
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>

---
+++ examples/transcoders/gzip_transcoder.rb
@@ -0,0 +1,49 @@
+# Author:: Couchbase <info@couchbase.com>
+# Copyright:: 2013 Couchbase, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'zlib'
+require 'stringio'
+
+# This class wraps any other transcoder and performs compression
+# using zlib
+class GzipTranscoder
+  FMT_GZIP = 0x04
+
+  def initialize(base = nil)
+    @base = base || Couchbase::Transcoder::Plain
+  end
+
+  def dump(obj, flags, options = {})
+    obj, flags = @base.dump(obj, flags, options)
+    io = StringIO.new
+    gz = Zlib::GzipWriter.new(io)
+    gz.write(obj)
+    gz.close
+    [io.string, flags|FMT_GZIP]
+  end
+
+  def load(blob, flags, options = {})
+    # decompress value only if gzip flag set
+    if (flags & FMT_GZIP) == FMT_GZIP
+      io = StringIO.new(blob)
+      gz = Zlib::GzipReader.new(io)
+      blob = gz.read
+      gz.close
+    end
+    @base.load(blob, flags, options)
+  end
+end
---
+++ examples/transcoders/options.rb
@@ -0,0 +1,54 @@
+# Author:: Couchbase <info@couchbase.com>
+# Copyright:: 2013 Couchbase, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'optparse'
+
+# just output extra empty line on CTRL-C
+trap("INT") do
+  STDERR.puts
+  exit
+end
+
+OPTIONS = {
+  :bucket => "default",
+  :hostname => "127.0.0.1",
+  :port => 8091,
+  :username => nil,
+  :password => nil
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options] keys"
+  opts.on("-h", "--hostname HOSTNAME", "Hostname to connect to (default: #{OPTIONS[:hostname]}:#{OPTIONS[:port]})") do |v|
+    host, port = v.split(':')
+    OPTIONS[:hostname] = host.empty? ? '127.0.0.1' : host
+    OPTIONS[:port] = port.to_i > 0 ? port.to_i : 8091
+  end
+  opts.on("-u", "--user USERNAME", "Username to log with (default: none)") do |v|
+    OPTIONS[:username] = v
+  end
+  opts.on("-p", "--password PASSWORD", "Password to log with (default: none)") do |v|
+    OPTIONS[:password] = v
+  end
+  opts.on("-b", "--bucket NAME", "Name of the bucket to connect to (default: #{OPTIONS[:bucket]})") do |v|
+    OPTIONS[:bucket] = v
+  end
+  opts.on_tail("-?", "--help", "Show this message") do
+    STDERR.puts opts
+    exit
+  end
+end.parse!
