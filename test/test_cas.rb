3085bc38e5661af29b8c79b7c43ded7b7a11c5cc
Scott W. Bradley <scottwb@scottwb.net>
2013-11-13 17:50:54 -0800

    Add test cases for the Couchbase::Bucket#cas failure condition that happens whena concurrent writer modifies the document between the #get and #set calls inside the #cas method.

--- test/test_cas.rb
+++ test/test_cas.rb
@@ -40,6 +40,23 @@ class TestCas < MiniTest::Test
     assert_equal expected, val
   end
 
+  def test_compare_and_swap_collision
+    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port,
+                               :default_format => :document)
+    connection.set(uniq_id, {"bar" => 1})
+    assert_raises(Couchbase::Error::KeyExists) do
+      connection.cas(uniq_id) do |val|
+        # Simulate collision with a separate writer. This will
+        # change the CAS value to be different than what #cas just loaded.
+        connection.set(uniq_id, {"bar" => 2})
+
+        # Complete the modification we desire, which should fail when set.
+        val["baz"] = 3
+        val
+      end
+    end
+  end
+
   def test_compare_and_swap_async
     connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port,
                                :default_format => :document)
@@ -64,6 +81,36 @@ class TestCas < MiniTest::Test
     val = connection.get(uniq_id)
     expected = {"bar" => 1, "baz" => 2}
     assert_equal expected, val
+  end
+
+  def test_compare_and_swap_async_collision
+    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port,
+                               :default_format => :document)
+    connection.set(uniq_id, {"bar" => 1})
+    calls = 0
+    connection.run do |conn|
+      conn.cas(uniq_id) do |ret|
+        calls += 1
+        case ret.operation
+        when :get
+          new_val = ret.value
+
+          # Simulate collision with a separate writer. This will
+          # change the CAS value to be different than what #cas just loaded.
+          connection.set(uniq_id, {"bar" => 2})
+
+
+          # Complete the modification we desire, which should fail when set.
+          new_val["baz"] = 3
+          new_val
+        when :set
+          assert ret.error.is_a? Couchbase::Error::KeyExists
+        else
+          flunk "Unexpected operation: #{ret.operation.inspect}"
+        end
+      end
+    end
+    assert_equal 2, calls
   end
 
   def test_flags_replication
