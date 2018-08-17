25ff2958d58df0850025d510111788cfaa23946a
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-08-17 13:18:00 +0300

    RCBC-205: Migrate Bucket#touch to API v3
    Change-Id: I4a3d4752f3540ffe6e565ab9a8b3f9259472245e
    Reviewed-on: http://review.couchbase.org/98433
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- ext/couchbase_ext/bucket.c
+++ ext/couchbase_ext/bucket.c
@@ -1,6 +1,6 @@
 /* vim: ft=c et ts=8 sts=4 sw=4 cino=
  *
- *   Copyright 2011, 2012 Couchbase, Inc.
+ *   Copyright 2011-2018 Couchbase, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -282,7 +282,6 @@ do_connect(struct cb_bucket_st *bucket)
     lcb_set_cookie(bucket->handle, bucket);
     (void)lcb_set_bootstrap_callback(bucket->handle, bootstrap_callback);
     (void)lcb_set_get_callback(bucket->handle, cb_get_callback);
-    (void)lcb_set_touch_callback(bucket->handle, cb_touch_callback);
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_HTTP, cb_http_callback);
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_OBSERVE, cb_observe_callback);
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_STORE, cb_storage_callback);
@@ -292,6 +291,7 @@ do_connect(struct cb_bucket_st *bucket)
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_STATS, cb_stat_callback);
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_COUNTER, cb_arithmetic_callback);
     (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_UNLOCK, cb_unlock_callback);
+    (void)lcb_install_callback3(bucket->handle, LCB_CALLBACK_TOUCH, cb_touch_callback);
 
     lcb_cntl(bucket->handle, (bucket->timeout > 0) ? LCB_CNTL_SET : LCB_CNTL_GET, LCB_CNTL_OP_TIMEOUT,
              &bucket->timeout);
--- ext/couchbase_ext/couchbase_ext.h
+++ ext/couchbase_ext/couchbase_ext.h
@@ -374,11 +374,6 @@ VALUE cb_encode_value(VALUE transcoder, VALUE val, uint32_t *flags, VALUE option
 VALUE cb_decode_value(VALUE transcoder, VALUE blob, uint32_t flags, VALUE options);
 
 void cb_get_callback(lcb_t handle, const void *cookie, lcb_error_t error, const lcb_get_resp_t *resp);
-void cb_touch_callback(lcb_t handle, const void *cookie, lcb_error_t error, const lcb_touch_resp_t *resp);
-void cb_http_complete_callback(lcb_http_request_t request, lcb_t handle, const void *cookie, lcb_error_t error,
-                               const lcb_http_resp_t *resp);
-void cb_http_data_callback(lcb_http_request_t request, lcb_t handle, const void *cookie, lcb_error_t error,
-                           const lcb_http_resp_t *resp);
 void cb_observe_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 void cb_storage_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 void cb_remove_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
@@ -386,6 +381,7 @@ void cb_version_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 void cb_stat_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 void cb_arithmetic_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 void cb_unlock_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
+void cb_touch_callback(lcb_t handle, int cbtype, const lcb_RESPBASE *rb);
 
 VALUE cb_get_transcoder(struct cb_bucket_st *bucket, VALUE override, int compat, VALUE opts);
 
