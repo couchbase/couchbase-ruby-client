162eb683db017bf05a030f4b52c03fbb097c0237
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-08-20 18:22:37 +0300

    RCBC-205: Fix tests for Bucket#delete
    Change-Id: I1508e18197c99602ccace0543fd51ab7169b05ff
    Reviewed-on: http://review.couchbase.org/98519
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- test/test_delete.rb
+++ test/test_delete.rb
@@ -1,5 +1,5 @@
 # Author:: Couchbase <info@couchbase.com>
-# Copyright:: 2011-2017 Couchbase, Inc.
+# Copyright:: 2011-2018 Couchbase, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,96 +15,128 @@
 # limitations under the License.
 #
 
-require File.join(File.dirname(__FILE__), 'setup')
+require File.join(__dir__, 'setup')
 
 class TestStore < MiniTest::Test
-  def setup
-    @mock = start_mock
-  end
-
-  def teardown
-    stop_mock(@mock)
-  end
-
   def test_trivial_delete
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    connection.set(uniq_id, "bar")
-    assert connection.delete(uniq_id)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.delete(uniq_id)
-    end
+    connection = Couchbase.new(mock.connstr)
+    refute connection.set(uniq_id, "bar").error
+
+    res = connection.delete(uniq_id)
+    refute res.error
+    assert_kind_of Numeric, res.cas
+
+    res = connection.delete(uniq_id)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
   end
 
   def test_delete_missing
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.delete(uniq_id(:missing))
-    end
-    refute connection.delete(uniq_id(:missing), :quiet => true)
-    refute connection.quiet?
-    connection.quiet = true
-    refute connection.delete(uniq_id(:missing))
+    connection = Couchbase.new(mock.connstr)
+    res = connection.delete(uniq_id(:missing))
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
   end
 
   def test_delete_with_cas
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    cas = connection.set(uniq_id, "bar")
-    missing_cas = cas - 1
-    assert_raises(Couchbase::Error::KeyExists) do
-      connection.delete(uniq_id, :cas => missing_cas)
-    end
-    assert connection.delete(uniq_id, :cas => cas)
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id, "bar")
+    refute res.error
+    cas = res.cas
+    missing_cas = res.cas + 1
+
+    res = connection.delete(uniq_id, :cas => missing_cas)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_EEXISTS', res.error.name
+
+    res = connection.delete(uniq_id, :cas => cas)
+    refute res.error
+    assert_kind_of Numeric, res.cas
   end
 
   def test_allow_fixnum_as_cas_parameter
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    cas = connection.set(uniq_id, "bar")
-    assert connection.delete(uniq_id, cas)
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id, "bar")
+    refute res.error
+    cas = res.cas
+
+    res = connection.delete(uniq_id, cas)
+    refute res.error
+    assert_kind_of Numeric, res.cas
   end
 
   def test_simple_multi_delete
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
-    res = connection.delete(uniq_id(1), uniq_id(2))
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    refute res[uniq_id(1)].error
+    refute res[uniq_id(2)].error
+
+    res = connection.delete([uniq_id(1), uniq_id(2)])
     assert res.is_a?(Hash)
-    assert res[uniq_id(1)]
-    assert res[uniq_id(2)]
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    refute res[uniq_id(2)].error
+    assert_kind_of Numeric, res[uniq_id(2)].cas
   end
 
   def test_simple_multi_delete_missing
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
-    res = connection.delete(uniq_id(1), uniq_id(:missing), :quiet => true)
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    refute res[uniq_id(1)].error
+    refute res[uniq_id(2)].error
+
+    res = connection.delete([uniq_id(1), uniq_id(:missing)])
     assert res.is_a?(Hash)
-    assert res[uniq_id(1)]
-    refute res[uniq_id(:missing)]
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    assert_instance_of Couchbase::LibraryError, res[uniq_id(:missing)].error
+    assert_equal 'LCB_KEY_ENOENT', res[uniq_id(:missing)].error.name
   end
 
   def test_multi_delete_with_cas_check
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    cas = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
-    res = connection.delete(uniq_id(1) => cas[uniq_id(1)], uniq_id(2) => cas[uniq_id(2)])
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    refute res[uniq_id(2)].error
+    assert_kind_of Numeric, res[uniq_id(2)].cas
+
+    res = connection.delete(uniq_id(1) => res[uniq_id(1)].cas, uniq_id(2) => res[uniq_id(2)].cas)
     assert res.is_a?(Hash)
-    assert res[uniq_id(1)]
-    assert res[uniq_id(2)]
+    refute res[uniq_id(1)].error
+    refute res[uniq_id(2)].error
   end
 
   def test_multi_delete_missing_with_cas_check
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    cas = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
-    res = connection.delete(uniq_id(1) => cas[uniq_id(1)], uniq_id(:missing) => cas[uniq_id(2)])
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    refute res[uniq_id(2)].error
+    assert_kind_of Numeric, res[uniq_id(2)].cas
+
+    res = connection.delete(uniq_id(1) => res[uniq_id(1)].cas, uniq_id(:missing) => res[uniq_id(2)].cas)
     assert res.is_a?(Hash)
-    assert res[uniq_id(1)]
-    refute res[uniq_id(:missing)]
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    assert_instance_of Couchbase::LibraryError, res[uniq_id(:missing)].error
+    assert_equal 'LCB_KEY_ENOENT', res[uniq_id(:missing)].error.name
   end
 
   def test_multi_delete_with_cas_check_mismatch
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    cas = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    connection = Couchbase.new(mock.connstr, :quiet => true)
+    res = connection.set(uniq_id(1) => "bar", uniq_id(2) => "foo")
+    refute res[uniq_id(1)].error
+    assert_kind_of Numeric, res[uniq_id(1)].cas
+    refute res[uniq_id(2)].error
+    assert_kind_of Numeric, res[uniq_id(2)].cas
 
-    assert_raises(Couchbase::Error::KeyExists) do
-      connection.delete(uniq_id(1) => cas[uniq_id(1)] + 1,
-                        uniq_id(2) => cas[uniq_id(2)])
-    end
+    res = connection.delete(uniq_id(1) => res[uniq_id(1)].cas + 1,
+                            uniq_id(2) => res[uniq_id(2)].cas)
+    assert res.is_a?(Hash)
+    assert_instance_of Couchbase::LibraryError, res[uniq_id(1)].error
+    assert_equal 'LCB_KEY_EEXISTS', res[uniq_id(1)].error.name
+    refute res[uniq_id(2)].error
+    assert_kind_of Numeric, res[uniq_id(2)].cas
   end
 end
