0380e13692b81c550221e5f42a46a5de28eb2043
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-08-20 20:33:53 +0300

    RCBC-205: Fix tests for Bucket#touch
    Change-Id: I31343fd446fef2a9806bdceaf97887b9e5e66e49
    Reviewed-on: http://review.couchbase.org/98536
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- test/test_touch.rb
+++ test/test_touch.rb
@@ -1,5 +1,5 @@
 # Author:: Couchbase <info@couchbase.com>
-# Copyright:: 2011-2017 Couchbase, Inc.
+# Copyright:: 2011-2018 Couchbase, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,81 +15,68 @@
 # limitations under the License.
 #
 
-require File.join(File.dirname(__FILE__), 'setup')
+require File.join(__dir__, 'setup')
 
 class TestTouch < MiniTest::Test
-  def setup
-    @mock = start_mock
-  end
-
-  def teardown
-    stop_mock(@mock)
-  end
-
   def test_trivial_touch
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    connection.set(uniq_id, "bar", :ttl => 1)
-    connection.touch(uniq_id, :ttl => 2)
-    sleep(1)
-    assert connection.get(uniq_id)
-    sleep(2)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.get(uniq_id)
-    end
+    connection = Couchbase.new(mock.connstr)
+    refute connection.set(uniq_id, "bar", :ttl => 1).error
+    refute connection.touch(uniq_id, :ttl => 3).error
+
+    mock.time_travel(2)
+
+    res = connection.get(uniq_id)
+    refute res.error
+
+    mock.time_travel(4)
+
+    res = connection.get(uniq_id)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
   end
 
   def test_multi_touch
-    connection = Couchbase.new(:port => @mock.port)
-    connection.set(uniq_id(1), "bar")
-    connection.set(uniq_id(2), "baz")
-    ret = connection.touch(uniq_id(1) => 1, uniq_id(2) => 1)
-    assert ret[uniq_id(1)]
-    assert ret[uniq_id(2)]
-    sleep(2)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.get(uniq_id(1))
-    end
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.get(uniq_id(2))
+    connection = Couchbase.new(mock.connstr)
+    refute connection.set(uniq_id(1), "bar").error
+    refute connection.set(uniq_id(2), "baz").error
+
+    res = connection.touch(uniq_id(1) => 1, uniq_id(2) => 1)
+    refute res[uniq_id(1)].error
+    refute res[uniq_id(2)].error
+
+    mock.time_travel(2)
+
+    [1, 2].each do |suff|
+      res = connection.get(uniq_id(suff))
+      assert_instance_of Couchbase::LibraryError, res.error
+      assert_equal 'LCB_KEY_ENOENT', res.error.name
     end
   end
 
   def test_it_uses_default_ttl_for_touch
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :default_ttl => 1)
-    connection.set(uniq_id, "bar", :ttl => 10)
-    connection.touch(uniq_id)
-    sleep(2)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.get(uniq_id)
-    end
+    connection = Couchbase.new(mock.connstr, :default_ttl => 1)
+    refute connection.set(uniq_id, "bar", :ttl => 10).error
+    refute connection.touch(uniq_id).error
+
+    mock.time_travel(2)
+
+    res = connection.get(uniq_id)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
   end
 
   def test_it_accepts_ttl_for_get_command
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    connection.set(uniq_id, "bar", :ttl => 10)
-    val = connection.get(uniq_id, :ttl => 1)
-    assert_equal "bar", val
-    sleep(2)
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.get(uniq_id)
-    end
-  end
+    connection = Couchbase.new(mock.connstr)
+    refute connection.set(uniq_id, "bar", :ttl => 10).error
 
-  def test_missing_in_quiet_mode
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :quiet => true)
-    cas1 = connection.set(uniq_id(1), "foo1")
-    cas2 = connection.set(uniq_id(2), "foo2")
+    res = connection.get(uniq_id, :ttl => 1)
+    refute res.error
+    assert_equal "bar", res.value
 
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.touch(uniq_id(:missing), :quiet => false)
-    end
+    mock.time_travel(2)
 
-    val = connection.touch(uniq_id(:missing))
-    refute(val)
-
-    ret = connection.touch(uniq_id(1), uniq_id(:missing), uniq_id(2))
-    assert_equal true, ret[uniq_id(1)]
-    assert_equal false, ret[uniq_id(:missing)]
-    assert_equal true, ret[uniq_id(2)]
+    res = connection.get(uniq_id)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
   end
 end
