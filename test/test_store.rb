635f7a1b31de80bf2a47bc1430822bc07d0561c7
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-08-20 19:46:00 +0300

    RCBC-205: Fix tests for Bucket#set operations
    Change-Id: I7bbc24915de3237f12bd8ab540eda1eeb3ae5812
    Reviewed-on: http://review.couchbase.org/98524
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- test/test_store.rb
+++ test/test_store.rb
@@ -1,5 +1,5 @@
 # Author:: Couchbase <info@couchbase.com>
-# Copyright:: 2011-2017 Couchbase, Inc.
+# Copyright:: 2011-2018 Couchbase, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,86 +15,93 @@
 # limitations under the License.
 #
 
-require File.join(File.dirname(__FILE__), 'setup')
+require File.join(__dir__, 'setup')
 
 class TestStore < MiniTest::Test
-  def setup
-    @mock = start_mock
-  end
-
-  def teardown
-    stop_mock(@mock)
-  end
-
   def test_trivial_set
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
-    cas = connection.set(uniq_id, "bar")
-    assert(cas > 0)
+    connection = Couchbase.new(mock.connstr)
+    res = connection.set(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
   end
 
   def test_set_with_cas
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
-    cas1 = connection.set(uniq_id, "bar1")
-    assert cas1 > 0
+    res = connection.set(uniq_id, "bar1")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas1 = res.cas
 
-    assert_raises(Couchbase::Error::KeyExists) do
-      connection.set(uniq_id, "bar2", :cas => cas1 + 1)
-    end
+    res = connection.set(uniq_id, "bar2", :cas => cas1 + 1)
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_EEXISTS', res.error.name
 
-    cas2 = connection.set(uniq_id, "bar2", :cas => cas1)
-    assert cas2 > 0
+    res = connection.set(uniq_id, "bar2", :cas => cas1)
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas2 = res.cas
     refute_equal cas2, cas1
 
-    cas3 = connection.set(uniq_id, "bar3")
-    assert cas3 > 0
+    res = connection.set(uniq_id, "bar3")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas3 = res.cas
+
     refute_equal cas3, cas2
     refute_equal cas3, cas1
   end
 
   def test_add
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
-    cas1 = connection.add(uniq_id, "bar")
-    assert cas1 > 0
+    res = connection.add(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas = res.cas
 
-    assert_raises(Couchbase::Error::KeyExists) do
-      connection.add(uniq_id, "bar")
-    end
+    res = connection.add(uniq_id, "bar")
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_EEXISTS', res.error.name
 
-    assert_raises(Couchbase::Error::KeyExists) do
-      connection.add(uniq_id, "bar", :cas => cas1)
+    assert_raises(ArgumentError) do
+      connection.add(uniq_id, "bar", :cas => cas)
     end
   end
 
   def test_replace
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
-    assert_raises(Couchbase::Error::NotFound) do
-      connection.replace(uniq_id, "bar")
-    end
+    res = connection.replace(uniq_id, "bar")
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_KEY_ENOENT', res.error.name
 
-    cas1 = connection.set(uniq_id, "bar")
-    assert cas1 > 0
+    res = connection.set(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
 
-    connection.replace(uniq_id, "bar")
+    res = connection.replace(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
   end
 
   def test_acceptable_keys
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
-    cas = connection.set(uniq_id.to_sym, "bar")
-    assert cas > 0
+    res = connection.set(uniq_id.to_sym, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
 
-    cas = connection.set(uniq_id.to_s, "bar")
-    assert cas > 0
+    res = connection.set(uniq_id.to_s, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
 
-    assert_raises(TypeError) do
+    assert_raises(ArgumentError) do
       connection.set(nil, "bar")
     end
 
     obj = {:foo => "bar", :baz => 1}
-    assert_raises(TypeError) do
+    assert_raises(ArgumentError) do
       connection.set(obj, "bar")
     end
 
@@ -102,108 +109,149 @@ class TestStore < MiniTest::Test
       alias_method :to_str, :to_s
     end
 
-    connection.set(obj, "bar")
-    assert cas > 0
+    # does not matter if object can act as string implicitly
+    assert_raises(ArgumentError) do
+      connection.set(obj, "bar")
+    end
   end
 
   def test_it_raises_error_when_appending_or_prepending_to_missing_key
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
-    assert_raises(Couchbase::Error::NotStored) do
-      connection.append(uniq_id(:missing), "foo")
-    end
+    res = connection.append(uniq_id(:missing), "foo")
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_NOT_STORED', res.error.name
 
-    assert_raises(Couchbase::Error::NotStored) do
-      connection.prepend(uniq_id(:missing), "foo")
-    end
+    res = connection.prepend(uniq_id(:missing), "foo")
+    assert_instance_of Couchbase::LibraryError, res.error
+    assert_equal 'LCB_NOT_STORED', res.error.name
   end
 
   def test_append
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :default_format => :plain)
+    connection = Couchbase.new(mock.connstr, :default_format => :plain)
 
-    cas1 = connection.set(uniq_id, "foo")
-    assert cas1 > 0
-    cas2 = connection.append(uniq_id, "bar")
-    assert cas2 > 0
+    res = connection.set(uniq_id, "foo")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas1 = res.cas
+
+    res = connection.append(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas2 = res.cas
+
     refute_equal cas2, cas1
 
-    val = connection.get(uniq_id)
-    assert_equal "foobar", val
+    res = connection.get(uniq_id)
+    refute res.error
+    assert_equal "foobar", res.value
   end
 
   def test_prepend
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :default_format => :plain)
+    connection = Couchbase.new(mock.connstr, :default_format => :plain)
 
-    cas1 = connection.set(uniq_id, "foo")
-    assert cas1 > 0
-    cas2 = connection.prepend(uniq_id, "bar")
-    assert cas2 > 0
+    res = connection.set(uniq_id, "foo")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas1 = res.cas
+
+    res = connection.prepend(uniq_id, "bar")
+    refute res.error
+    assert_operator res.cas, :>, 0
+    cas2 = res.cas
+
     refute_equal cas2, cas1
 
-    val = connection.get(uniq_id)
-    assert_equal "barfoo", val
-  end
-
-  def test_set_with_prefix
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :key_prefix => "prefix:")
-    connection.set(uniq_id(:foo), "bar")
-    assert_equal "bar", connection.get(uniq_id(:foo))
-    expected = {uniq_id(:foo) => "bar"}
-    assert_equal expected, connection.get(uniq_id(:foo), :assemble_hash => true)
-
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :key_prefix => nil)
-    expected = {"prefix:#{uniq_id(:foo)}" => "bar"}
-    assert_equal expected, connection.get("prefix:#{uniq_id(:foo)}", :assemble_hash => true)
+    res = connection.get(uniq_id)
+    refute res.error
+    assert_equal "barfoo", res.value
   end
 
   ArbitraryData = Struct.new(:baz)
 
   def test_set_using_brackets
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
 
     connection[uniq_id(1)] = "foo"
-    val = connection.get(uniq_id(1))
-    assert_equal "foo", val
 
-    if RUBY_VERSION =~ /^1\.9/
-      eval <<-EOC
-      connection[uniq_id(2), :flags => 0x1100] = "bar"
-      val, flags = connection.get(uniq_id(2), :extended => true)
-      assert_equal "bar", val
-      assert_equal 0x1100, flags
+    res = connection.get(uniq_id(1))
+    refute res.error
+    assert_equal "foo", res.value
 
-      connection[uniq_id(3), :format => :marshal] = ArbitraryData.new("thing")
-      val = connection.get(uniq_id(3))
-      assert val.is_a?(ArbitraryData)
-      assert_equal "thing", val.baz
-      EOC
-    end
+    connection[uniq_id(3), :format => :marshal] = ArbitraryData.new("thing")
+
+    res = connection.get(uniq_id(3))
+    refute res.error
+    assert_instance_of ArbitraryData, res.value
+    assert_equal "thing", res.value.baz
   end
 
   def test_multi_store
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port, :default_format => :plain)
-    connection.add(uniq_id(:a) => "bbb", uniq_id(:z) => "yyy")
-    assert_equal %w(bbb yyy), connection.get(uniq_id(:a), uniq_id(:z))
+    connection = Couchbase.new(mock.connstr, :default_format => :plain)
 
-    connection.prepend(uniq_id(:a) => "aaa", uniq_id(:z) => "xxx")
-    assert_equal %w(aaabbb xxxyyy), connection.get(uniq_id(:a), uniq_id(:z))
+    res = connection.add(uniq_id(:a) => "bbb", uniq_id(:z) => "yyy")
+    refute res[uniq_id(:a)].error
+    refute res[uniq_id(:z)].error
 
-    connection.append(uniq_id(:a) => "ccc", uniq_id(:z) => "zzz")
-    assert_equal %w(aaabbbccc xxxyyyzzz), connection.get(uniq_id(:a), uniq_id(:z))
+    res = connection.get([uniq_id(:a), uniq_id(:z)])
+    refute res[uniq_id(:a)].error
+    assert_equal 'bbb', res[uniq_id(:a)].value
+    refute res[uniq_id(:z)].error
+    assert_equal 'yyy', res[uniq_id(:z)].value
 
-    connection.replace(uniq_id(:a) => "foo", uniq_id(:z) => "bar")
-    assert_equal %w(foo bar), connection.get(uniq_id(:a), uniq_id(:z))
+    res = connection.prepend(uniq_id(:a) => "aaa", uniq_id(:z) => "xxx")
+    refute res[uniq_id(:a)].error
+    refute res[uniq_id(:z)].error
+
+    res = connection.get([uniq_id(:a), uniq_id(:z)])
+    refute res[uniq_id(:a)].error
+    assert_equal 'aaabbb', res[uniq_id(:a)].value
+    refute res[uniq_id(:z)].error
+    assert_equal 'xxxyyy', res[uniq_id(:z)].value
+
+    res = connection.append(uniq_id(:a) => "ccc", uniq_id(:z) => "zzz")
+    refute res[uniq_id(:a)].error
+    refute res[uniq_id(:z)].error
+
+    res = connection.get([uniq_id(:a), uniq_id(:z)])
+    refute res[uniq_id(:a)].error
+    assert_equal 'aaabbbccc', res[uniq_id(:a)].value
+    refute res[uniq_id(:z)].error
+    assert_equal 'xxxyyyzzz', res[uniq_id(:z)].value
+
+    res = connection.replace(uniq_id(:a) => "foo", uniq_id(:z) => "bar")
+    refute res[uniq_id(:a)].error
+    refute res[uniq_id(:z)].error
+
+    res = connection.get([uniq_id(:a), uniq_id(:z)])
+    refute res[uniq_id(:a)].error
+    assert_equal 'foo', res[uniq_id(:a)].value
+    refute res[uniq_id(:z)].error
+    assert_equal 'bar', res[uniq_id(:z)].value
 
     res = connection.set(uniq_id(:a) => "bar", uniq_id(:z) => "foo")
-    assert_equal %w(bar foo), connection.get(uniq_id(:a), uniq_id(:z))
-    assert res.is_a?(Hash)
+    refute res[uniq_id(:a)].error
+    refute res[uniq_id(:z)].error
+
+    res = connection.get([uniq_id(:a), uniq_id(:z)])
+    refute res[uniq_id(:a)].error
+    assert_equal 'bar', res[uniq_id(:a)].value
+    refute res[uniq_id(:z)].error
+    assert_equal 'foo', res[uniq_id(:z)].value
   end
 
   def test_append_format
-    connection = Couchbase.new(:hostname => @mock.host, :port => @mock.port)
+    connection = Couchbase.new(mock.connstr)
     assert_equal :document, connection.default_format
-    connection.set(uniq_id, 'bar', :format => :plain)
-    connection.append(uniq_id, 'baz', :format => :plain)
-    assert_equal 'barbaz', connection.get(uniq_id)
+
+    res = connection.set(uniq_id, 'bar', :format => :plain)
+    refute res.error
+
+    res = connection.append(uniq_id, 'baz', :format => :plain)
+    refute res.error
+
+    res = connection.get(uniq_id)
+    refute res.error
+    assert_equal 'barbaz', res.value
   end
 end
