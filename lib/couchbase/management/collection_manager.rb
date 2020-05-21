436f29923c963c07ce837ec5894b7190a0a514aa
Sergey Avseyev <sergey.avseyev@gmail.com>
2020-05-22 01:56:23 +0300

    📝 add documentation for collection manager
    Change-Id: I5ce69e921d3080028a020a6bd1f23b89b2e16ea6
    Reviewed-on: http://review.couchbase.org/c/couchbase-ruby-client/+/128843
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- lib/couchbase/management/collection_manager.rb
+++ lib/couchbase/management/collection_manager.rb
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+require "couchbase/errors"
+
 module Couchbase
   module Management
     class CollectionManager
@@ -22,6 +24,148 @@ module Couchbase
       def initialize(backend, bucket_name)
         @backend = backend
         @bucket_name = bucket_name
+      end
+
+      # Get all scopes
+      #
+      # @param [GetAllScopesOptions] options
+      #
+      # @return [Array<ScopeSpec>]
+      def get_all_scopes(options = GetAllScopesOptions.new)
+        # GET /pools/default/buckets/{bucket}/collections
+      end
+
+      def get_scope(scope_name, options = GetScopeOptions.new) end
+
+      # Creates a new scope
+      #
+      # @param [String] scope_name name of the scope
+      # @param [CreateScopeOptions] options
+      #
+      # @raise [ArgumentError]
+      def create_scope(scope_name, options = CreateScopeOptions.new)
+        # POST /pools/default/buckets/{bucket}/collections -d name={scope_name}
+      end
+
+      # Removes a scope
+      #
+      # @param [String] scope_name name of the scope
+      # @param [DropScopeOptions] options
+      #
+      # @raise [Error::ScopeNotFound]
+      def drop_scope(scope_name, options = DropScopeOptions.new)
+        # DELETE /pools/default/buckets/{bucket}/collections/{scope_name}
+      end
+
+      # Creates a new collection
+      #
+      # @param [CollectionSpec] collection specification of the collection
+      # @param [CreateCollectionOptions] options
+      #
+      # @raise [ArgumentError]
+      # @raise [Error::CollectionExist]
+      # @raise [Error::ScopeNotFound]
+      def create_collection(collection, options = CreateCollectionOptions.new)
+        # POST /pools/default/buckets/{bucket}/collections/{scope_name} -d name={collection_name} -D maxTTL={maxTTL}
+      end
+
+      # Removes a collection
+      #
+      # @param [CollectionSpec] collection specification of the collection
+      # @param [DropCollectionOptions] options
+      #
+      # @raise [Error::CollectionNotFound]
+      def drop_collection(collection, options = DropCollectionOptions.new)
+        # DELETE /pools/default/buckets/{bucket}/collections/{scope_name}/{collection_name}
+      end
+
+      class GetScopeOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [GetScopeOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+
+      class GetAllScopesOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [GetAllScopesOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+
+      class CreateScopeOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [CreateScopeOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+
+      class DropScopeOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [DropScopeOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+
+      class CreateCollectionOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [CreateCollectionOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+
+      class DropCollectionOptions
+        # @return [Integer] the time in milliseconds allowed for the operation to complete
+        attr_accessor :timeout
+
+        # @yieldparam [DropCollectionOptions] self
+        def initialize
+          yield self if block_given?
+        end
+      end
+    end
+
+    class ScopeSpec
+      # @return [String] name of the scope
+      attr_accessor :name
+
+      # @return [Array<CollectionSpec>] list of collections associated with the scope
+      attr_reader :collections
+
+      # @yieldparam [ScopeSpec] self
+      def initialize
+        yield self if block_given?
+      end
+    end
+
+    class CollectionSpec
+      # @return [String] name of the collection
+      attr_accessor :name
+
+      # @return [String] name of the scope
+      attr_accessor :scoionpe_name
+
+      # @return [Integer] time in seconds of the expiration for new documents in the collection (set to +nil+ to disable it)
+      attr_accessor :max_expiry
+
+      # @yieldparam [CollectionSpec] self
+      def initialize
+        yield self if block_given?
       end
     end
   end
