f45dacba91a307ae5db9f96658f89d5aa9169bee
Dimitris Christodoulou <dimitris.christodoulou@couchbase.com>
2022-09-27 14:19:48 +0100

    Add Configuration Profiles

---
+++ lib/couchbase/config_profiles.rb
@@ -0,0 +1,58 @@
+#  Copyright 2020-2022 Couchbase, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+module Couchbase
+  module ConfigProfiles
+    class Profiles
+      attr :profiles
+
+      def initialize
+        @profiles = Hash.new
+        register_profile("wan_development", DevelopmentProfile.new)
+      end
+
+      def register_profile(name, profile)
+        @profiles[name] = profile
+      end
+
+      def apply(profile_name, options)
+        if @profiles.key?(profile_name)
+          @profiles[profile_name].apply(options)
+          return
+        end
+        raise ArgumentError, "#{profile_name} is not a registered profile"
+      end
+    end
+
+    class Profile
+      def apply(options); end
+    end
+
+    class DevelopmentProfile < Profile
+      def apply(options)
+        options.key_value_timeout = 20 * 1000
+        # TODO Add `options.key_value_durable_timeout = 20 * 1000` when key_value_durable_timeout is added to Options::Cluster
+        options.connect_timeout = 20 * 1000
+        options.view_timeout = 120 * 1000
+        options.query_timeout = 120 * 1000
+        options.analytics_timeout = 120 * 1000
+        options.search_timeout = 120 * 1000
+        options.management_timeout = 120 * 1000
+      end
+    end
+
+    KNOWN_PROFILES = Profiles.new
+
+  end
+end
