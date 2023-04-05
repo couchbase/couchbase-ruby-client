d51a6ea5db9b28112089a3dba6927d99da90b0de
Dimitris Christodoulou <36637689+DemetrisChr@users.noreply.github.com>
2023-04-05 15:55:43 +0100

    Add search (#90)

---
+++ lib/couchbase/protostellar/response_converter/search.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Couchbase
+  module Protostellar
+    module ResponseConverter
+      class Search
+        def self.to_search_result(resp)
+          # TODO: Convert the response to a search result
+          raise "Not implemented yet"
+        end
+      end
+    end
+  end
+end
