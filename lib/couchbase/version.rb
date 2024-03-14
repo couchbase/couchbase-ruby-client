1bc6c0a4abffbd0e53bbf681218ea4d103f4d2c8
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-03-14 13:26:22 -0700

    update meta for 3.5.0

--- lib/couchbase/version.rb
+++ lib/couchbase/version.rb
@@ -19,5 +19,5 @@ module Couchbase
   #   $ ruby -rcouchbase -e 'pp Couchbase::VERSION'
   #   {:sdk=>"3.4.0", :ruby_abi=>"3.1.0", :revision=>"416fe68e6029ec8a4c40611cf6e6b30d3b90d20f"}
   VERSION = {} unless defined?(VERSION) # rubocop:disable Style/MutableConstant
-  VERSION.update(:sdk => "3.4.6.snapshot".freeze)
+  VERSION.update(:sdk => "3.5.0".freeze)
 end
